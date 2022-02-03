236599d2894ac753b6899645abb4712499cee15f
Michael <58919638+michaaels@users.noreply.github.com>
2022-02-03 18:08:01 -0500

    Update csgo_fix_blackscreen.sh

--- csgo_fix_blackscreen.sh
+++ csgo_fix_blackscreen.sh
@@ -4,5 +4,5 @@ sudo dnf install -y gperftools-libs # Just in case we don't have the library yet
 
 sudo mkdir -p /usr/legacy/lib64
 sudo ln -sf /usr/lib64/libtcmalloc_minimal.so.4.5.9 /usr/legacy/lib64/libtcmalloc_minimal.so.0
-cd ~/.local/share/Steam/steamapps/common/Counter-Strike\ Global\ Offensive # Installation directory of csgo
+cd ~/.local/share/Steam/steamapps/common/Counter-Strike\ Global\ Offensive # Installation directory of CSGO
 sed -i 's|export LD_LIBRARY_PATH="${GAMEROOT}"/bin:"${GAMEROOT}"/bin/linux64:$LD_LIBRARY_PATH|export LD_LIBRARY_PATH=/usr/legacy/lib64:"${GAMEROOT}"/bin:"${GAMEROOT}"/bin/linux64:$LD_LIBRARY_PATH|g' csgo.sh
